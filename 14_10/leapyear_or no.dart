21316d89add2a651bda21ef5a52f8dbcc23a996d
Aesha-Faldu <132562552+Aesha-Faldu@users.noreply.github.com>
2023-10-17 03:54:04 -0700

    Update leapyear_or no.dart

--- 14_10/leapyear_or no.dart
+++ 14_10/leapyear_or no.dart
@@ -4,7 +4,7 @@ void main(){
 
   int year = 1700;
 
-  if((year%4==0) && (year%400==0) )
+  ifif(((year%400==0)||(year%100!=0)) && year % 4 == 0 )
       print("$year year is Leapyear");
   else
     print("$year year is  Not Leapyear");
