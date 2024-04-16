075a402df54dad3a45b5dcf42c48490befdc0418
Aesha-Faldu <132562552+Aesha-Faldu@users.noreply.github.com>
2024-04-16 17:26:19 +0530

    Update rashiModel.dart

--- Assesment-2/Home/rashiModel.dart
+++ Assesment-2/Home/rashiModel.dart
@@ -9,30 +9,33 @@ class RashiModel extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    return ListView.builder(
-        itemCount:list.length,
-        itemBuilder:(context,i)
-        {
-          return ListTile
-            (
-            leading: Image.network(list[i]["img"].toString(),
-            height: 20,
-              width: 20,
+    return Expanded(
+      child: ListView.builder(
+          itemCount:list.length,
+          itemBuilder:(context,i)
+          {
+            return ListTile
+              (
+              leading: Image.network(list[i]["img"].toString(),
+              height: 20,
+                width: 20,
+                fit: BoxFit.cover,
 
-            ),
+              ),
 
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("id: ${list[i]['id'].toString()}"),
-                Text("title: ${list[i]['title'].toString()}"),
-                Text("subtile: ${list[i]['subtile'].toString()}"),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("id: ${list[i]['id'].toString()}"),
+                  Text("title: ${list[i]['title'].toString()}"),
+                  Text("subtile: ${list[i]['subtile'].toString()}"),
 
-              ],
-            ),
+                ],
+              ),
 
-          );
-        });
+            );
+          }),
+    );
   }
 
 }
